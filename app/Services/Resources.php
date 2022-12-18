f51a05eaf99b64d14f569a7936e5f4520ca0dfaa
Aija Pastare <aija.pastare@gmail.com>
2022-12-18 20:15:32 +0200

    Refactoring

--- app/Services/Resources.php
+++ app/Services/Resources.php
@@ -8,10 +8,12 @@ use GuzzleHttp\Client;
 
 class Resources
 {
+    const FOURTEENTH_SAEIMA = 14;
     const BASE_URL = 'https://data.gov.lv';
     const RESOURCE_SAEIMAS_SEDES = '/dati/dataset/saeimas-sedes';
     const CLASS_RESOURCE_LIST = 'resource-list';
     const CLASS_HEADING = 'heading';
+    const ATTRIBUTE_DATA_ID = 'data-id';
     const ATTRIBUTE_HREF = 'href';
     const ATTRIBUTE_TITLE = 'title';
     const FILE_EXTENSION = 'xml';
@@ -19,7 +21,7 @@ class Resources
 
     protected $resourceList = [];
 
-    public function getResourceList()
+    public function getResourceList(): array
     {
         $httpClient = new Client();
 
@@ -41,23 +43,30 @@ class Resources
 
             $title = $headingNode->getAttribute(self::ATTRIBUTE_TITLE);
 
-            if (!str_contains($title, self::FILE_CATEGORY)) {
+            if (!str_contains($title, self::FILE_CATEGORY) || strtok($title, '.') < self::FOURTEENTH_SAEIMA) {
                 continue;
             }
 
-            $fileName = preg_replace('#[ -]+#', '-', $title);
-
-            if (substr($fileName, -3) === strtoupper(self::FILE_EXTENSION)) {
-                $fileName = substr($fileName, 0, -3) . '.' . self::FILE_EXTENSION;
-            } else {
-                $fileName = $fileName . '.' . self::FILE_EXTENSION;
-            }
-
+            $fileName = $this->getFileName($title);
+            $dataId = $resourceNode->getAttribute(self::ATTRIBUTE_DATA_ID);
             $href = $headingNode->getAttribute(self::ATTRIBUTE_HREF);
 
-            $this->resourceList[] = self::BASE_URL . $href . '/download/' . $fileName;
+            $this->resourceList[$dataId] = self::BASE_URL . $href . '/download/' . $fileName;
         }
 
         return $this->resourceList;
+    }
+
+    private function getFileName(string $title): string
+    {
+        $fileName = preg_replace('#[ -]+#', '-', $title);
+
+        if (substr($fileName, -3) === strtoupper(self::FILE_EXTENSION)) {
+            $fileName = substr($fileName, 0, -3) . '.' . self::FILE_EXTENSION;
+        } else {
+            $fileName = $fileName . '.' . self::FILE_EXTENSION;
+        }
+
+        return $fileName;
     }
 }
